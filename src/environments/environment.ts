4db1d36f27187a61cbe4bb60e1d3f6c4ca75bef2
ClaudioSnow <31218693+ClaudioSnow@users.noreply.github.com>
2018-11-29 13:59:54 -0200

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,35 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-export const environment = {
-  production: false
+/**
+ * export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC4kIrtP10r6a4BcMfg9GEaTAXnx08vFvA",
+    authDomain: "yourfood-5ee84.firebaseapp.com",
+    databaseURL: "https://yourfood-5ee84.firebaseio.com",
+    projectId: "yourfood-5ee84",
+    storageBucket: "yourfood-5ee84.appspot.com",
+    messagingSenderId: "939614885044"
+  }
+
 };
+ */
+
+  // Initialize Firebase
+  export const environment ={
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyCuaEN7fJ0WdgNUmSQv5Y_X_0ILSbPq02s",
+  authDomain: "yourfood-72c53.firebaseapp.com",
+  databaseURL: "https://yourfood-72c53.firebaseio.com",
+  projectId: "yourfood-72c53",
+  storageBucket: "yourfood-72c53.appspot.com",
+  messagingSenderId: "1046082850276"
+  }
+};
+
+
 
 /*
  * In development mode, to ignore zone related error stack frames such as
